f4b24b9384caeef76fb07df9287efed74b817d44
Marcus Badillo <hello@mediaduk.com>
2019-08-17 21:22:48 -0700

    Click event added to menu hamburger button.

--- angular-src/src/app/component/header/header.component.ts
+++ angular-src/src/app/component/header/header.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'Todo App One';
+
+  menuOpen = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  animateHamburger(open: boolean) {
+    console.log(`Menu is open: ${open}`);
+    if (open) {
+
+    } else {
+
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen
+    this.animateHamburger(this.menuOpen)
   }
 
 }
