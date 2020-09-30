6426a0653ab7959e0f4f73d3778227ddb3d8564c
Marcus Badillo <marcusb003@hotmail.com>
2020-09-29 19:00:39 -0500

    Todo form submits

--- client/src/app/component/add-todo/add-todo.component.ts
+++ client/src/app/component/add-todo/add-todo.component.ts
@@ -1,41 +1,39 @@
-
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Todo } from '../../../../../models/Todo';
-import { TodoService } from '../../service/todo.service'
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Todo } from "../../../../../models/Todo";
+import { TodoService } from "../../service/todo.service";
 
 @Component({
-  selector: 'app-add-todo',
-  templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.scss']
+  selector: "app-add-todo",
+  templateUrl: "./add-todo.component.html",
+  styleUrls: ["./add-todo.component.scss"],
 })
 export class AddTodoComponent implements OnInit {
   private newTodo: Todo;
-  constructor(private todoServ: TodoService) { }
+  constructor(private todoServ: TodoService) {}
 
   @Output() addTodo: EventEmitter<Todo> = new EventEmitter<Todo>();
 
   ngOnInit() {
-    this.clearForm()
+    this.clearForm();
   }
 
   public clearForm() {
     this.newTodo = {
-      _id: '',
-      title: '',
-      description: '',
-      priority: '',
-    }
+      _id: "",
+      title: "",
+      description: "",
+      priority: "",
+    };
   }
 
   public onSubmit() {
-    this.todoServ.addTodo(this.newTodo).subscribe(
-      response => {
-        const res = JSON.parse(response);
-        if (res.success == true) {
-          this.addTodo.emit(this.newTodo);
-          this.clearForm()
-        }
+    this.todoServ.addTodo(this.newTodo).subscribe((response) => {
+      const res = JSON.parse(response);
+      console.log(res);
+      if (res.success == true) {
+        this.addTodo.emit(this.newTodo);
+        this.clearForm();
       }
-    );
+    });
   }
 }
--- client/src/app/component/modal-create-todo/modal-create-todo.component.ts
+++ client/src/app/component/modal-create-todo/modal-create-todo.component.ts
@@ -1,79 +1,53 @@
-import { Component, OnInit, Output, EventEmitter, HostBinding } from '@angular/core';
-import { Todo } from '../../../../../models/Todo'
-import { TodoService } from '../../service/todo.service'
-import { ModalCreateTodoService } from '../../service/modal-create-todo.service'
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Todo } from "../../../../../models/Todo";
+import { TodoService } from "../../service/todo.service";
+import { ModalCreateTodoService } from "../../service/modal-create-todo.service";
 
 @Component({
-  selector: 'app-modal-create-todo',
-  templateUrl: './modal-create-todo.component.html',
-  styleUrls: ['./modal-create-todo.component.scss']
+  selector: "app-modal-create-todo",
+  templateUrl: "./modal-create-todo.component.html",
+  styleUrls: ["./modal-create-todo.component.scss"],
 })
 export class ModalCreateTodoComponent implements OnInit {
-  private newTodo: Todo;
-  public modalOpen: boolean = false;
-  constructor(private todoService: TodoService, private modalCreateTodoService: ModalCreateTodoService) { }
+  constructor(
+    private todoService: TodoService,
+    private modalCreateTodoService: ModalCreateTodoService
+  ) {}
   @Output() addTodo: EventEmitter<Todo> = new EventEmitter<Todo>();
 
+  private newTodo: Todo;
+  public modalOpen: boolean = false;
+
   ngOnInit() {
-    this.clearForm()
-    this.modalCreateTodoService.change.subscribe(state => this.modalOpen = state);
+    this.clearForm();
+    this.modalCreateTodoService.change.subscribe(
+      (state) => (this.modalOpen = state)
+    );
   }
 
   public clearForm() {
     this.newTodo = {
-      _id: '',
-      title: '',
-      description: '',
-      priority: '',
-    }
+      _id: "",
+      title: "",
+      description: "",
+      priority: "",
+    };
   }
 
-  public setPriorityClassBtns(p: string) {
-    const getClassList = id => document.getElementById(`priority${id}`).classList,
-      disable = (id: string) => {
-        const classList = getClassList(id)
-        classList.add('disable')
-      },
-      enable = (id: string) => {
-        const classList = getClassList(id)
-        classList.remove('disable')
+  createTodo() {
+    console.log("onSubmit() ----------");
+
+    console.log(this.newTodo);
+    this.todoService.addTodo(this.newTodo).subscribe((response) => {
+      const res = JSON.parse(response);
+      console.log(res);
+      if (res.success == true) {
+        this.addTodo.emit(this.newTodo);
+        this.clearForm();
       }
-    if (p === 'High') {
-      enable('High')
-      disable('Medium')
-      disable('Low')
-    } else if (p === 'Medium') {
-      disable('High')
-      enable('Medium')
-      disable('Low')
-    } else {
-      disable('High')
-      disable('Medium')
-      enable('Low')
-    }
-  }
-  public setPriority(priority: string) {
-    this.newTodo.priority = priority;
-    this.setPriorityClassBtns(priority);
-  }
+    });
 
-  public createTodo() {
-    console.log('createTodo() ----------')
-  }
-
-  public toggleModal(state) {
-    this.modalCreateTodoService.toggle(state);
-  }
-
-  public onSubmit() {
-    this.todoService.addTodo(this.newTodo).subscribe(
-      response => {
-        const res = JSON.parse(response);
-        if (res.success == true) {
-          this.addTodo.emit(this.newTodo);
-          this.clearForm()
-        }
-      }
-    );
+    this.modalCreateTodoService.toggle(false);
   }
 }
